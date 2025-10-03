5896a2a555cd2ca6ab49c7326bcd5723649924fc
Santosh <121541404+Immortalsantosh@users.noreply.github.com>
2025-10-03 07:42:42 +0530

    Update auth.controllers.ts

--- Backend/src/Controllers/auth.controllers.ts
+++ Backend/src/Controllers/auth.controllers.ts
@@ -34,7 +34,7 @@ export const signUp = async (req: Request, res: Response): Promise<void> => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "None",
+      sameSite: "none",
       secure: true, // set true if HTTPS
     });
 
@@ -66,7 +66,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "None",
+      sameSite: "none",
       secure: true,
     });
 
